130354cf6ffe2cf1b8f248d23e632d22be576b58
jazz287 <78864460+jazz287@users.noreply.github.com>
2023-10-13 19:48:14 +0530

    ..

--- findPerson.java
+++ findPerson.java
@@ -1,3 +1,4 @@
+import java.lang.Math.*;
 public class findPerson{
 public static void main (String args[]){
 
@@ -6,11 +7,36 @@ p1.name = "Sanya";
 System.out.println(p1.name);
 p1.age = 42;
 System.out.println(p1.age);
-}
 
+Circle c1 = new Circle();
+c1.radius = 4;
+System.out.println("Perimeter is : " + c1.perimeter(4));
+
+System.out.println("Area is : " + c1.area(4));
+}
 }
 
 class Person{
     String name;
     int age;
 }
+class Circle{
+    
+    int radius;
+    double perimeter(int radius){
+        double per = 2*3.14*radius;
+        return per;
+    }
+
+        double area(int radius) {
+            double Area = 3.14*radius*radius;
+            return Area;
+        }
+    
+
+    
+
+    
+        
+    }
+
